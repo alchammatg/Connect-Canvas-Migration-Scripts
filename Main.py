c5c697c3cbc03993375db565b7058b8416a68a67
alchammatg <alchammatg@gmail.com>
2018-05-19 14:52:04 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,3 @@
-#Settings
-import Init
-
 #Course entry and login
 from Login import tokenLogin
 from Enter_Course import enterCourse
@@ -12,7 +9,9 @@ from Modulate import MasterModulate
 from Uniquify_Titles import MasterUniquifyTitles
 from Fix_Internal_Links import fixInternalLinks
 from API_Calls import setHome
-import services
+import Services
+
+################################################
 
 #Log in and enter a course
 tokenLogin()
@@ -21,7 +20,6 @@ enterCourse()
 #coverts HTML files to pages and fixes links to internal files/images/content_pages
 MasterFTP()
 
-#Prompts user to break up a module, tries to uniquify repeated titles of pages (ex:readings, readings-2, ..), cleans html content of each page from specified items
 MasterClean()
 
 #Sets course home page

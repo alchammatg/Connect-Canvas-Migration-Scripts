ab47de95436e29c24e552c2969072d68b469d8f2
alchammatg <alchammatg@gmail.com>
2018-05-19 13:54:43 -0700

    Delete Delete_Unpublished_Pages.py

--- services/Delete_Unpublished_Pages.py
+++
@@ -1,7 +0,0 @@
-import API_Calls as API
-
-def deleteUnpublishedPages():
-    unpublished_pages = API.getAllUnpublishedPages()
-    for unpublished_page in unpublished_pages:
-        print(unpublished_page['title'])
-        API.deletePage(unpublished_page['url'])

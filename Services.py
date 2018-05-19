6a3c59dd758e4015c727de0247e122da955fc0db
alchammatg <alchammatg@gmail.com>
2018-05-19 13:56:00 -0700

    Update Services.py

--- Services.py
+++ Services.py
@@ -35,3 +35,21 @@ def MasterRAP():
         restorePage(page['title'], page['url'])
     Init.q.put(["finished",True])
     return
+
+def unduplicateModuleContents(moduleID):
+    module_items = API.getModuleItems(moduleID)
+    checked_items = []
+    for module_item in module_items:
+        data_pair = (module_item['title'], module_item['type'])
+        if data_pair not in checked_items:
+            checked_items.append(data_pair)
+        else:
+            print("----Repeated item detected: [{}]".format(data_pair))
+            API.deleteModuleItem(moduleID, module_item['id'])
+
+
+def MasterUMC():
+    modules = API.getAllModules()
+    for module in modules:
+        print("Processing Module: ({})".format(module['name']))
+        unduplicateModuleContents(module['id'])
